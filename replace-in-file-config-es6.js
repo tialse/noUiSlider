072f4bbd02d7dfc80d69bf5caeea0b5d6a04db3b
Alexandr Tiron <tiron@ukr.net>
2021-05-06 12:56:09 +0300

    replace V to _V

--- replace-in-file-config-es6.js
+++ replace-in-file-config-es6.js
@@ -1,9 +1,11 @@
 module.exports = {
     files: 'dist/nouislider.js',
     from: [
-        '%%REPLACE_THIS_WITH_VERSION%%'
+        '%%REPLACE_THIS_WITH_VERSION%%',
+        /VERSION/g
     ],
     to: [
-        process.env.npm_package_version
+        process.env.npm_package_version,
+        '_VERSION'
     ]
 }
